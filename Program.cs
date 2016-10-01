5f41724d41586abfd3c75d913c32bf2eaa9f4968
Danil Shy <udavster@gmail.com>
2016-10-01 15:06:55 +0300

    Finished. Program is finding circular prime numbers and counting them.

--- Program.cs
+++ Program.cs
@@ -7,36 +7,56 @@ using System.Threading.Tasks;
 namespace CircularPrime {
     class Program {
         static void Main(string[] args) {
-            CircularPrime.FindPrimeList(100);
-            if ("abc".Contains("a")) Console.WriteLine("a");
-
+            Console.WriteLine("Started finding circular prime numbers");
+            int[] cPrime = CircularPrime.Find(1000000);
+            Console.WriteLine("Done");
+            for (int i = 0; i < cPrime.Length; i++) {
+                Console.Write("{0}\t", cPrime[i]);
+                if ((i+1) % 10 == 0) {
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("\n\nNumber of circular prime numbers: {0}", cPrime.Length);
             Console.ReadLine();
         }
     }
     class CircularPrime {
-        public static int[] Find(int searchUntilNum) {
-            List<int> prime = FindPrimeList(searchUntilNum);
-            List<int> circularPrime = new List<int>();
-            foreach (int num in prime) {
-                if (num < 10) {
-                    circularPrime.Add(num);
+        public static int[] Find(UInt32 searchUntilNum) {
+            List<int> prime = GetPrimeList(searchUntilNum);
+            List<string> sPrime = prime.ConvertAll<string>(delegate(int i) { return i.ToString(); });
+            List<string> sCircularPrime = new List<string>();
+            foreach (string sNum in sPrime) {
+                if (sNum.Length < 2) {
+                    sCircularPrime.Add(sNum);
+                    continue;
                 }
-                string sNum = num.ToString();
                 if ((sNum.Contains("2")) || (sNum.Contains("4")) || (sNum.Contains("6")) || (sNum.Contains("8"))
                     || (sNum.Contains("0"))) {
                     continue;
                 }
-
+                if (checkIfCircular(sNum, sPrime)) {
+                    sCircularPrime.Add(sNum);
+                } 
             }
-            return null;
+            return sCircularPrime.ConvertAll<int>(delegate(string str) { return Int32.Parse(str); }).ToArray();
+        }
+        private static bool checkIfCircular(string sNum, List<string> list) {
+            for (int i = 0; i < sNum.Length; i++) {
+                sNum = sNum[sNum.Length - 1] + sNum.Substring(0, sNum.Length - 1);
+                if (!list.Contains(sNum)) {
+                    return false;
+                }     
+            }
+            return true;
         }
         // Sieve of Eratosthenes
-        public static List<int> FindPrimeList(int searchUntilNum) {
+        private static List<int> GetPrimeList(UInt32 searchUntilNum) {
             bool[] num = new bool[searchUntilNum + 1];
             //added one extra bool to array - 0 to make code more readable -
             //we can use i and j without -1 each time
             List<int> prime = new List<int>();
             double upperLimit = Math.Pow(searchUntilNum, 0.5);
+            num[1] = true; //1 is not prime number (https://en.wikipedia.org/wiki/Prime_number)
             for (int i = 2; i < upperLimit; i++) {
                 if (num[i]) continue;
                 for (int j = i * i; j <= searchUntilNum; j += i) {
